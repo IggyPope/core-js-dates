8c860d3507a2f218e813d6f20dc97b51e519b061
IggyPope <altko4egar@gmail.com>
2024-03-24 15:22:55 +0300

    feat: implement getNextFriday task solution

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -61,8 +61,10 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  return new Date(
+    date.setDate(date.getDate() + (((7 + 5 - date.getDay() - 1) % 7) + 1))
+  );
 }
 
 /**
